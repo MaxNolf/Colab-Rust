0b98ae4139e45c2f9a41cda1032209fc4c36bc43
MaxNolf <gamesonlinega@gmail.com>
2026-02-25 23:30:17 -0300

    chore: remove ignored binaries and build artifacts

--- template_github/target/release/build/target-lexicon-1a439dbf04cdafc2/build_script_build-1a439dbf04cdafc2.d
+++
@@ -1,8 +0,0 @@
-C:\Codigos\Rust\Colab-Rust-Bridge\template_github\target\release\build\target-lexicon-1a439dbf04cdafc2\build_script_build-1a439dbf04cdafc2.d: C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\target-lexicon-0.12.16\build.rs C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\target-lexicon-0.12.16\src/data_model.rs C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\target-lexicon-0.12.16\src/triple.rs C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\target-lexicon-0.12.16\src/targets.rs
-
-C:\Codigos\Rust\Colab-Rust-Bridge\template_github\target\release\build\target-lexicon-1a439dbf04cdafc2\build_script_build-1a439dbf04cdafc2.exe: C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\target-lexicon-0.12.16\build.rs C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\target-lexicon-0.12.16\src/data_model.rs C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\target-lexicon-0.12.16\src/triple.rs C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\target-lexicon-0.12.16\src/targets.rs
-
-C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\target-lexicon-0.12.16\build.rs:
-C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\target-lexicon-0.12.16\src/data_model.rs:
-C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\target-lexicon-0.12.16\src/triple.rs:
-C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\target-lexicon-0.12.16\src/targets.rs:
--- template_github/target/release/deps/cfg_if-be2711f84a777e73.d
+++
@@ -1,7 +0,0 @@
-C:\Codigos\Rust\Colab-Rust-Bridge\template_github\target\release\deps\cfg_if-be2711f84a777e73.d: C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Codigos\Rust\Colab-Rust-Bridge\template_github\target\release\deps\libcfg_if-be2711f84a777e73.rlib: C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Codigos\Rust\Colab-Rust-Bridge\template_github\target\release\deps\libcfg_if-be2711f84a777e73.rmeta: C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Users\luc4o\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs:
